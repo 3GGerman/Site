02568074fff3ec09f5ad90221498b74b3e6848da
3GGerman <german.diablo2@gmail.com>
2023-04-17 09:20:43 +0300

    Убрал лишнее из views.py(по типу неиспользованных переменных) и добавил представление DetailView там же. Так же создал detail.html и base.html. Домашней страницей сделал имя домена(то есть 127.0.0.1 это имя локалхоста)

---
+++ movie_keys/movies/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+
+from .models import Movies
+
+class IndexView(generic.ListView):
+    """Представление списка всех фильмов что есть в базе"""
+    template_name = 'movies/index.html'
+    context_object_name = 'movies_list'
+
+    def get_queryset(self):
+        """Возвращает все опубликованные фильмы на сайте"""
+        more_movie = Movies.objects.all()
+        for i in range(3):
+            more_movie = more_movie.union(more_movie, all=True)
+        return more_movie
+
+
+class DetailView(generic.DetailView):
+    model = Movies
+    template_name = 'movies/detail.html'
+
