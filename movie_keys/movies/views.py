b136bc2172153669d042d6813a3d24b13c22544a
3GGerman <german.diablo2@gmail.com>
2023-04-20 17:18:50 +0300

    Чутка переделал вьюху для индексаХТМЛ

--- movie_keys/movies/views.py
+++ movie_keys/movies/views.py
@@ -4,25 +4,36 @@ from django.views import generic
 from .models import Movies
 
 
-class IndexView(generic.ListView):
-    """Представление списка всех фильмов что есть в базе"""
+"""class IndexView(generic.ListView):
     template_name = 'movies/index.html'
     context_object_name = 'movies_list'
 
     def get_queryset(self):
-        """Возвращает все опубликованные фильмы на сайте"""
+        
         all_movies = Movies.objects.all()
         return all_movies
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['counter'] = [x for x in range(250)]
-        return context
+        return context"""
+
+
+def index(request):
+    all_movies = Movies.objects.all()
+    context = {
+        'all_movies': all_movies,
+        'counter': [x for x in range(250)],
+    }
+    return render(request, 'movies/index.html', context=context)
 
 
 def detail(request, movie_slug):
     movie = get_object_or_404(Movies, slug=movie_slug)
-    return render(request, 'movies/detail.html', {'movie': movie})
+    context = {
+        'movie': movie
+    }
+    return render(request, 'movies/detail.html', context=context)
 
 
 #Тест view
