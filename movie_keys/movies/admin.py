e3b3378a95254ca81bf38f4207277ea0497d2cf9
3GGerman <german.diablo2@gmail.com>
2023-04-20 17:47:03 +0300

    Добавил фильтрацию в фильмах в админке

--- movie_keys/movies/admin.py
+++ movie_keys/movies/admin.py
@@ -9,6 +9,7 @@ class MoviesAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'poster', 'date_create', 'is_published']
     list_display_links = ['id', 'title']
     search_fields = ['title']
+    list_filter = ['is_published', 'date_create']
 
 
 admin.site.register(Movies, MoviesAdmin)
