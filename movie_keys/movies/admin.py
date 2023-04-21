5910daac453a4b91e0671e51baad0f914fe6b289
3GGerman <german.diablo2@gmail.com>
2023-04-21 23:09:32 +0300

    Вернул некоторые файлы чтоб админка работала на русском

--- movie_keys/movies/admin.py
+++ movie_keys/movies/admin.py
@@ -8,6 +8,9 @@ class MoviesAdmin(admin.ModelAdmin):
     """Для отображения доп.инфо. списка фильмов"""
     list_display = ['id', 'title', 'poster', 'date_create', 'is_published']
     list_display_links = ['id', 'title']
+    search_fields = ['title']
+    list_filter = ['is_published', 'date_create']
+
 
 
 admin.site.register(Movies, MoviesAdmin)
