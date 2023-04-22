b515deca5c6dcf4a0f8c3ff5c7e1ede42d9179af
3GGerman <german.diablo2@gmail.com>
2023-04-22 13:01:12 +0300

    Обновил базу данных

--- movie_keys/movies/models.py
+++ movie_keys/movies/models.py
@@ -11,7 +11,7 @@ class Movies(models.Model):
     about = models.TextField(blank=True, verbose_name='О фильме')
     date_create = models.DateTimeField(auto_now=True, verbose_name='Дата добавления')
     is_published = models.BooleanField(default=True, verbose_name='Публикация')
-    slug = models.SlugField(max_length=30, unique=True)
+    slug = models.SlugField(max_length=30, unique=True, db_index=True, verbose_name='URL')
 
     def __str__(self):
         return self.title
